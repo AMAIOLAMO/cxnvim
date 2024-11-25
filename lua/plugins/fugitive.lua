2c3007134cf66be49eb40737933753f66c2230bc
CxRedix <realcxredix@gmail.com>
2024-11-25 09:07:02 +0800

    Created convenient keybind for toggling fugitive git buffer (my first actual nvim lua function!)

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,6 +1,14 @@
 return {
     "tpope/vim-fugitive",
     init = function()
-        vim.keymap.set('n', '<leader>g', vim.cmd.Git)
+        vim.keymap.set("n", "<M-g>", function()
+            if vim.g.fugitive_git_buffer == nil then
+                vim.cmd("vert Git")
+                vim.g.fugitive_git_buffer = vim.fn.bufnr(vim.fn.bufname(".git"))
+            else
+                vim.cmd.bdelete { vim.g.fugitive_git_buffer }
+                vim.g.fugitive_git_buffer = nil
+            end
+        end, { desc = "Toggle Fugitive Git Window" })
     end
 }
