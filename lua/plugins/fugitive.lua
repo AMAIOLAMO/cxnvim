8560c91849a581640abdc7e87d5ec27a642b5421
CxRedix <realcxredix@gmail.com>
2024-11-25 09:38:56 +0800

    toggling fugitive git uses windows instead of buffers

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,13 +1,19 @@
+local function fugitive_get_git_winnr()
+    return vim.fn.bufwinnr(".git")
+end
+
+local function fugitive_git_win_exists()
+    return fugitive_get_git_winnr() ~= -1
+end
+
 return {
     "tpope/vim-fugitive",
     init = function()
         vim.keymap.set("n", "<M-g>", function()
-            if vim.g.fugitive_git_buffer == nil then
-                vim.cmd("vert Git")
-                vim.g.fugitive_git_buffer = vim.fn.bufnr(vim.fn.bufname(".git"))
+            if fugitive_git_win_exists() == true then
+                vim.cmd( fugitive_get_git_winnr() .. "close" )
             else
-                vim.cmd.bdelete { vim.g.fugitive_git_buffer }
-                vim.g.fugitive_git_buffer = nil
+                vim.cmd("vert Git")
             end
         end, { desc = "Toggle Fugitive Git Window" })
     end
