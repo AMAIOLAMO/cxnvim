d7fcc88a317143467a7fcc5478cbcfcf45b45165
CxRedix <realcxredix@gmail.com>
2024-12-15 11:13:00 +0800

    replaced primitive LSP reference finder with Telescope LSP references

--- lua/plugins/lsp_zero.lua
+++ lua/plugins/lsp_zero.lua
@@ -14,7 +14,7 @@ return {
             vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
             vim.keymap.set('n', '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
             vim.keymap.set('n', '<leader>ra', function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set('n', '<leader>cr', function() vim.lsp.buf.references() end, opts)
+            -- vim.keymap.set('n', '<leader>cr', function() vim.lsp.buf.references() end, opts)
 
             -- lsp_zero.default_keymaps({buffer = bufnr, remap = false})
         end)
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,15 +6,17 @@ return {
         "nvim-lua/plenary.nvim"
     },
     init = function()
-        local builtin = require('telescope.builtin')
+        local builtin = require("telescope.builtin")
 
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Telescope Find Files"})
-        vim.keymap.set('n', '<leader>fl', builtin.live_grep,  {desc = "Telescope Live Grep"})
-        vim.keymap.set('n', '<leader>fm', builtin.man_pages,  {desc = "Telescope Man Pages"})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags,  {desc = "Telescope Help Tags"})
-        vim.keymap.set('n', '<C-p>',      builtin.git_files,  {desc = "Telescope Git Files"})
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, {desc = "Telescope Find Files"})
+        vim.keymap.set("n", "<leader>fl", builtin.live_grep,  {desc = "Telescope Live Grep"})
+        vim.keymap.set("n", "<leader>fm", builtin.man_pages,  {desc = "Telescope Man Pages"})
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags,  {desc = "Telescope Help Tags"})
+        vim.keymap.set("n", "<C-p>",      builtin.git_files,  {desc = "Telescope Git Files"})
+        vim.keymap.set("n", "<leader>cr", builtin.lsp_references,  {desc = "Telescope LSP References"})
 
-        require('telescope').load_extension('fzf')
+
+        require("telescope").load_extension("fzf")
     end,
     opts = {
         pickers = {
